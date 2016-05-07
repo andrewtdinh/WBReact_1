385031a0c1f649076fa1c4a27ca30f9c7ce5ec66
Andrew Dinh <andrewdinh74@yahoo.com>
2016-05-06 23:27:30 -0700

    Display Hello on the page

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,15 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
 
 var StorePicker = React.createClass({
 
   render: function(){
-    return <p>hi</p>
+    return (
+      <p>hello</p>
+
+    )
   }
 
 });
+
+ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
