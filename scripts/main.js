d3a07412f67ed560d551b0d5487782f773252188
Andrew Dinh <andrewdinh74@yahoo.com>
2016-05-15 23:17:59 -0700

    Reset the form upon submit

--- scripts/main.js
+++ scripts/main.js
@@ -53,10 +53,11 @@ var AddFishForm = React.createClass({
 			image: this.refs.image.value
 		}
 		this.props.addFish(fish);
+		this.refs.fishForm.reset();
 	},
 	render: function(){
 		return (
-			<form className="fish-edit" onSubmit={this.createFish}>
+			<form className="fish-edit" ref="fishForm" onSubmit={this.createFish}>
 				<input type="text" ref="name" placeholder="Fish Name"/>
 				<input type="text" ref="price" placeholder="Fish Price"/>
 				<select ref="status">
