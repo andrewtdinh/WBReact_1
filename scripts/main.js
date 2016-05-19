75812ca0c792612cd0717295b6008d9c15e14de8
Andrew Dinh <andrewdinh74@yahoo.com>
2016-05-18 23:52:13 -0700

    Add buttonText variable

--- scripts/main.js
+++ scripts/main.js
@@ -59,6 +59,7 @@ var Fish = React.createClass({
 	render: function(){
 		var details = this.props.details;
 		var isAvailable = (details.status === 'available' ? true : false);
+		var buttonText = isAvailable ? 'Add To Order' : 'Sold Out!'
 		return (
 			<li className="menu-fish">
 				<img src={details.image} alt={details.name}/>
