ae67fdd0f48417ca54645f47381b9e83c15baceb
Andrew Dinh <andrewdinh74@yahoo.com>
2016-05-20 23:08:22 -0700

    Getting to the final format of the order

--- scripts/main.js
+++ scripts/main.js
@@ -142,8 +142,19 @@ var Header = React.createClass({
 
 var Order = React.createClass({
 	renderOrder: function(key){
+		var fish = this.props.fishes[key];
+		var count = this.props.order[key];
+
+		if (!fish){
+			return <li key={key}>Sorry, fish no longer available!</li>
+		}
+
 		return (
-			<li>{key}</li>
+			<li>
+				{count}lbs
+				{fish.name}
+				<span className="price">{h.formatPrice(count * fish.price)}</span>
+			</li>
 		)
 	},
 	render: function(){
