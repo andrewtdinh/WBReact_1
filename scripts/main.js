e1cc838fb41c3cdfa346cef12e82d4137ca3856e
Andrew Dinh <andrewdinh74@yahoo.com>
2016-05-20 22:34:05 -0700

    Return the order total correctly

--- scripts/main.js
+++ scripts/main.js
@@ -141,13 +141,19 @@ var Header = React.createClass({
 */
 
 var Order = React.createClass({
-	var orderIds = Object.keys(this.props.order);
-	var total = orderIds.reduce((prevTotal, key) => {
-		var fish = this.props.fish[key];
-		var count = this.props.order[key];
-		
-	});
 	render: function(){
+		var orderIds = Object.keys(this.props.order);
+		var total = orderIds.reduce((prevTotal, key) => {
+			var fish = this.props.fish[key];
+			var count = this.props.order[key];
+			var isAvailable = fish && fish.status === 'available';
+
+			if (fish && isAvailable){
+				return prevTotal + (count * parseInt(fish.price) || 0);
+			}
+
+			return prevTotal;
+		}, 0);
 		return (
 			<div className="order-wrap">
 				<h2 className="order-title">Your Order</h2>
