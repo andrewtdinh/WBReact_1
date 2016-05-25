21efbbae76646ba75bbdc2046efc9b2573befceb
Andrew Dinh <andrewdinh74@yahoo.com>
2016-05-24 18:28:05 -0700

    Fix bind error

--- scripts/main.js
+++ scripts/main.js
@@ -231,7 +231,7 @@ var Inventory = React.createClass({
 				</select>
 				<textarea valueLink={this.props.linkState('fishes.' +key+ '.desc')}></textarea>
 				<input type="text" valueLink={this.props.linkState('fishes.' +key+ '.image')} />
-				<button onClick={this.props.removeFish.bind(key)}>Remove Fish</button>
+				<button onClick={this.props.removeFish.bind(null, key)}>Remove Fish</button>
 			</div>
 		)
 	},
