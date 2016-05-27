6d9f6b5199df8753e9d4779366d45005d4dd5208
Andrew Dinh <andrewdinh74@yahoo.com>
2016-05-26 19:18:03 -0700

    Add removeButton in the order

--- scripts/main.js
+++ scripts/main.js
@@ -176,9 +176,10 @@ var Order = React.createClass({
 	renderOrder: function(key){
 		var fish = this.props.fishes[key];
 		var count = this.props.order[key];
+		var removeButton = <button onClick={this.props.removeFromOrder.bind(null, key)}>&times;</button>
 
 		if (!fish){
-			return <li key={key}>Sorry, fish no longer available!</li>
+			return <li key={key}>Sorry, fish no longer available! {removeButton}</li>
 		}
 
 		return (
@@ -186,6 +187,7 @@ var Order = React.createClass({
 				{count}lbs
 				{fish.name}
 				<span className="price">{h.formatPrice(count * fish.price)}</span>
+				{removeButton}
 			</li>
 		)
 	},
