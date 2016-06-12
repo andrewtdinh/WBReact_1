03a0c777d9421b7ec57da889bb179d584277b8dc
Andrew Dinh <andrewdinh74@yahoo.com>
2016-06-11 19:29:12 -0700

    Refactor functions inside StorePicker

--- scripts/components/StorePicker.js
+++ scripts/components/StorePicker.js
@@ -6,21 +6,23 @@ import {History} from 'react-router';
 import h from '../helpers';
 
 class StorePicker extends React.Component {
-	mixins: [History],
-	goToStore: function(event){
+	mixins: [History]
+
+	goToStore(event){
 		event.preventDefault();
 		var storeId = this.refs.storeId.value;
 		this.history.pushState(null, '/store/' + storeId);
-	},
-  	render: function(){
-    	return (
-    		<form className="store-selector" onSubmit={this.goToStore}>
-    			<h2>Please Enter a Store</h2>
-    			<input type="text" ref="storeId" defaultValue={h.getFunName()} />
-    			<input type="Submit" />
-    		</form>
-    	)
-  	}
+	}
+
+	render(){
+  	return (
+  		<form className="store-selector" onSubmit={this.goToStore}>
+  			<h2>Please Enter a Store</h2>
+  			<input type="text" ref="storeId" defaultValue={h.getFunName()} />
+  			<input type="Submit" />
+  		</form>
+  	)
+	}
 };
 
 export default StorePicker;
